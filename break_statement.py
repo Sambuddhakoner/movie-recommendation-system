62aa3259f5668e769f99e83f17b8f46ee29dc27c
Sambuddha <sambuddhakoner@gmail.com>
2023-04-21 00:18:23 +0530

    Create break_statement.py

---
+++ break_statement.py
@@ -0,0 +1,15 @@
+# for a in range(10):
+#     if a==5:
+#         # break;
+#         continue
+#     print(a)
+# else:
+#     print("This is inside else of for")
+
+# pass statement
+i=4
+if i>0:
+    pass
+while i>9:
+    pass
+print('good boi')
